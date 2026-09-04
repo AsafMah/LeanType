23461035295368d6b39b9e877f4136e7592c9df5
LeanBitLab <leanbitlab@users.noreply.github.com>
2026-09-05 01:37:32 +0530

    fix: commit typed word and finish composing text on editor actions

--- app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
+++ app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
@@ -2856,20 +2856,15 @@ public final class InputLogic {
     private void performEditorAction(final int actionId, final SettingsValues settingsValues,
             final LatinIME.UIHandler handler) {
         if (mWordComposer.isComposingWord()) {
-            final SuggestedWordInfo autoCorrection = mWordComposer.getAutoCorrectionOrNull();
             final String typedWord = mWordComposer.getTypedWord();
-            if (autoCorrection != null && !typedWord.equals(autoCorrection.mWord)) {
-                commitCurrentAutoCorrection(settingsValues, LastComposedWord.NOT_A_SEPARATOR, handler);
-            } else {
-                final NgramContext ngramContext = mConnection.getNgramContextFromNthPreviousWord(
-                        settingsValues.mSpacingAndPunctuations, 1);
-                performAdditionToUserHistoryDictionary(settingsValues, typedWord, ngramContext);
-                mLastComposedWord = mWordComposer.commitWord(
-                        LastComposedWord.COMMIT_TYPE_USER_TYPED_WORD, typedWord,
-                        LastComposedWord.NOT_A_SEPARATOR, ngramContext);
-                mConnection.finishComposingText();
-                StatsUtils.onWordCommitUserTyped(typedWord, mWordComposer.isBatchMode());
-            }
+            final NgramContext ngramContext = mConnection.getNgramContextFromNthPreviousWord(
+                    settingsValues.mSpacingAndPunctuations, 1);
+            performAdditionToUserHistoryDictionary(settingsValues, typedWord, ngramContext);
+            mLastComposedWord = mWordComposer.commitWord(
+                    LastComposedWord.COMMIT_TYPE_USER_TYPED_WORD, typedWord,
+                    LastComposedWord.NOT_A_SEPARATOR, ngramContext);
+            mConnection.finishComposingText();
+            StatsUtils.onWordCommitUserTyped(typedWord, mWordComposer.isBatchMode());
         }
         mConnection.performEditorAction(actionId);
     }
